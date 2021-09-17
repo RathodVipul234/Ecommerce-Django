f4f93e3dbbaca0c6d667d1c6edc8098f0528d060
Vipul Rathod <58567554+RathodVipul234@users.noreply.github.com>
2021-09-17 23:58:44 +0530

    Update settings.py

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'django-insecure-^(u3fw9o#*oqqtzmish1a#df27_%v-njob(9b9(62dh)ac(056
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
