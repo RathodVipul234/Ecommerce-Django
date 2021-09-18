56848de339460617910157629861df68e9b6e69e
Vipul Rathod <58567554+RathodVipul234@users.noreply.github.com>
2021-09-18 16:37:59 +0530

    Update settings.py

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -91,7 +91,7 @@ WSGI_APPLICATION = 'Ecommerce.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        
     }
 }
 
