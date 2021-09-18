9dad1def8c20980dbcf7e9a6e1434e9d6e68093a
Vipul Rathod <58567554+RathodVipul234@users.noreply.github.com>
2021-09-19 00:50:51 +0530

    Update settings.py

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -22,17 +22,17 @@ environ.Env.read_env(env_file=os.path.join(BASE_DIR,'.env') )
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 # email = os.environ.get('email')
-email=config("email")
+email=config("EMAIL")
 
 # password = os.environ.get('password')
-password=config("password")
+password=config("PASSWORD")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
